abc8aad4336f2ced94b20fb12527c466686ec560
RamyaSadhasivam <ramyaprabhu961@gmail.com>
2022-04-02 17:39:20 +0530

    added A

---
+++ src/test/java/GitClass/git/A.java
@@ -0,0 +1,14 @@
+package GitClass.git;
+
+public class A {
+
+public void method1() {
+System.out.println("1");	
+}
+public void method2() {
+	System.out.println("2");		
+}
+public void method3() {
+	System.out.println("3");		
+}
+}
